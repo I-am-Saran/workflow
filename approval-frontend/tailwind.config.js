442b63d48b5d0ea16349e639b46890b866d21a29
Saranraj S <900570@hepl.local>
2025-11-01 14:43:58 +0530

    req

---
+++ approval-frontend/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Adjust if you have a different folder structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
